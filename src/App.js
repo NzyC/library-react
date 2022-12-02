023324c36e248679ffd2b0d205b80925b48e782b
Nzy <nidal.chowdhury1@gmail.com>
2022-12-02 16:14:20 +1100

    upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Discounted from './components/Discounted';
+import Explore from './components/Explore';
+import Featured from './components/Featured';
+import Footer from './components/Footer';
+import Highlights from './components/Highlights';
+import Landing from './components/Landing';
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
+      <Explore />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
